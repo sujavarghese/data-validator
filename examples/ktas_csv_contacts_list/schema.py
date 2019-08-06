158c24b9c88ac0b46c0ba3beb0026fc46c9371b8
suja <sujacv@gmail.com>
2019-08-07 09:52:42 +1000

    Removed bad data

--- examples/ktas_csv_contacts_list/schema.py
+++
@@ -1,8 +0,0 @@
-from file_validator.schema.schema import FeatureSchema
-from examples.ktas_csv_contacts_list.validator import CustomRule
-
-
-class ContactsSchema(FeatureSchema):
-    map = {
-        'check_xyz': CustomRule
-    }
